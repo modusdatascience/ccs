4c6950b70d21224d90eb7cce86a5f78c3a9aaac0
Jason Rudy <jcrudy@gmail.com>
2017-04-22 19:20:21 -0700

    versioneer

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
+import versioneer
 
 setup(name='ccs',
-      version='0.1',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       author='Matt Lewis',
       url='https://github.com/mattlewissf/ccs',
       package_data={'ccs': ['resources/*']},
